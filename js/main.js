cba0c47ace5e3ce4cf145a665f91819af6ae7b0f
mobinveisy <55058921+mobinveisy@users.noreply.github.com>
2021-08-08 02:30:47 +0430

    courses card fix

--- js/main.js
+++ js/main.js
@@ -219,3 +219,23 @@ var swiper = new Swiper('.blog-slider', {
     clickable: true,
   }
 });
+
+
+    /* -----------------<< testimoial  Carousel >>--------------- */
+    $('.testimonials-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        autoplay: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 3,
+            }
+        }
+    });
