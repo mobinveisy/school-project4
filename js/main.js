7cd9a514ed0cad23dc2ccc9348f8b59da796d0e8
mobinveisy <55058921+mobinveisy@users.noreply.github.com>
2021-08-08 00:13:50 +0430

    board

--- js/main.js
+++ js/main.js
@@ -203,3 +203,19 @@ $(".dialogCorso .close,#backCover").click(function () {
   });
 
 });
+
+
+
+var swiper = new Swiper('.blog-slider', {
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: '.blog-slider__pagination',
+    clickable: true,
+  }
+});
