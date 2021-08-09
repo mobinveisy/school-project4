cf2981afbdf001b8e002f1cf77674c3f43e3d063
mobinveisy <55058921+mobinveisy@users.noreply.github.com>
2021-08-10 01:42:20 +0430

    fix console errors

--- js/main.js
+++ js/main.js
@@ -221,21 +221,21 @@ var swiper = new Swiper('.blog-slider', {
 });
 
 
-    /* -----------------<< testimoial  Carousel >>--------------- */
-    $('.testimonials-carousel').owlCarousel({
-        loop: true,
-        margin: 0,
-        autoplay: true,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1,
-            },
-            600: {
-                items: 2,
-            },
-            1000: {
-                items: 3,
-            }
-        }
-    });
+    // /* -----------------<< testimoial  Carousel >>--------------- */
+    // $('.testimonials-carousel').owlCarousel({
+    //     loop: true,
+    //     margin: 0,
+    //     autoplay: true,
+    //     responsiveClass: true,
+    //     responsive: {
+    //         0: {
+    //             items: 1,
+    //         },
+    //         600: {
+    //             items: 2,
+    //         },
+    //         1000: {
+    //             items: 3,
+    //         }
+    //     }
+    // });
